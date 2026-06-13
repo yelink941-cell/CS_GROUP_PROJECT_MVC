c24326f3210a245cb5fb1fdde445d511005722e5
KhineKhinWin <khinekhinwinkhinekhinwin@gmail.com>
2026-06-13 22:09:44 +0630

    khine

--- src/main/java/com/hibernate/controller/testing.java
+++ src/main/java/com/hibernate/controller/testing.java
@@ -3,5 +3,6 @@ package com.hibernate.controller;
 public class testing {
  public static void main(String[] args) {
 	System.out.println("hello  I am Ye Lin Kyaw");
+	System.out.println("Hello I am Khine");
 }
 }
