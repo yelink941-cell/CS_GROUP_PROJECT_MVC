3a25d933922df40221a20b410df4bf4895be5313
yelink941-cell <yelink941@gmail.com>
2026-06-13 23:18:39 +0630

    yk tesing

--- src/main/java/com/hibernate/controller/testing.java
+++ src/main/java/com/hibernate/controller/testing.java
@@ -4,5 +4,7 @@ public class testing {
  public static void main(String[] args) {
 	System.out.println("hello  I am Ye Lin Kyaw");
 	System.out.println("Hello I am Khine");
+	System.out.println("hello  I am Ye Lin Kyaw");
+
 }
 }
