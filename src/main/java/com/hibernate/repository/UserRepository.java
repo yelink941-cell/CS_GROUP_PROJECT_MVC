8af29191de701abb20bc4ca992081811e649cfeb
phonethitkoko7-glitch <phonethitkoko7@gmail.com>
2026-06-18 15:06:33 +0630

    hashpassword

--- src/main/java/com/hibernate/repository/UserRepository.java
+++ src/main/java/com/hibernate/repository/UserRepository.java
@@ -1,13 +1,15 @@
 package com.hibernate.repository;
 
 import com.hibernate.entity.User;
-import java.util.List;
-import java.util.Optional;
+import com.hibernate.entity.UserProfile;
+
+
 
 public interface UserRepository {
-    User save(User user);
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    Optional<User> findById(Long id);
-    List<User> searchByUsername(String keyword, Long excludeUserId, int limit);
+	void saveUser(User user);
+    void saveProfile(UserProfile profile);
+    boolean isEmailExists(String email);
+    User getUserByEmail(String email);
+    UserProfile getUserProfileByUserId(int userId);
+    void updateProfile(UserProfile profile);
 }	
