8dbd2fadc5d74d416d46acb8bc6b0d92a6de5f00
phonethitkoko7-glitch <phonethitkoko7@gmail.com>
2026-07-05 11:43:06 +0630

    for admin-dashboard

--- src/main/java/com/hibernate/controller/UserController.java
+++ src/main/java/com/hibernate/controller/UserController.java
@@ -381,6 +381,10 @@ public class UserController {
         
         return "admin/admin-user-management"; 
     }
+    @GetMapping("/admin/dashboard")
+    public String showAdminDashboard() {
+        return "admin/admin-dashboard"; 
+    }
 
     @PostMapping("/admin/users/update-status")
     public String updateStatusAndRole(
